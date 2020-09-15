4d0e0dde25cd8e3eace6a014bcb7fa342c2f88c7
Daniel Humer <danielhumer.at@icloud.com>
2020-09-16 00:40:50 +0200

    small improvements

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 number = int(input("Please enter a number between 1 and 100 to count to: "))
 
-for x in range(1, number+1):
-    if x % 3 == 0 and x % 5 == 0:
+for count in range(1, number+1):
+    if count % 3 == 0 and count % 5 == 0:
         print("fizzbuzz")
-    elif x % 3 == 0:
+    elif count % 3 == 0:
         print("fizz")
-    elif x % 5 == 0:
+    elif count % 5 == 0:
         print("buzz")
     else:
-        print(x)
+        print(count)
+
 
 
 
